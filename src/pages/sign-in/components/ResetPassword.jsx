65ac4f052687439cee57930f11ca0dd6bfaa0c4d
Rashmi Ranjan Nayak <rrnayak165@gmail.com>
2025-03-21 11:18:46 +0530

    remove : unused comment

--- src/pages/sign-in/components/ResetPassword.jsx
+++ src/pages/sign-in/components/ResetPassword.jsx
@@ -46,9 +46,6 @@ function ResetPassword() {
   const [alertMessage, setAlertMessage] = useState("");
 
   const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-
-  const token = queryParams.get("token");
 
   useEffect(() => {
     if (passwordResetSuccess) {
@@ -64,6 +61,8 @@ function ResetPassword() {
   }, [passwordResetError]);
 
   const handleFormSubmit = (data) => {
+    const queryParams = new URLSearchParams(location.search);
+    const token = queryParams.get("token");
     const reqData = {
       newPassword: data.confirmPassword,
       token: token,
@@ -82,295 +81,122 @@ function ResetPassword() {
     setOpenSnackbar(false);
   };
 
-    return (
-      <div className="reset-password-bg">
-        <Box
-          noValidate
-          component="form"
-          onSubmit={handleSubmit(handleFormSubmit)}
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            paddingX: 4,
-            gap: 2,
-            width: "100%",
-            maxWidth: "450px",
-          }}
-        >
-          <FormControl>
-            <FormLabel htmlFor="newPassword">New Password</FormLabel>
-            <TextField
-              error={!!errors?.newPassword} // Highlights the textfield with red border, if validation fails
-              helperText={errors?.newPassword?.message}
-              name="newPassword"
-              placeholder="Enter new password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              {...register("newPassword", {
-                required: {
-                  value: true,
-                  message: "Please enter the password",
-                },
-                pattern: {
-                  value: /^(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]+$/,
-                  message:
-                    "Password must contain at least one digit and one special character",
-                },
-                minLength: {
-                  value: 8,
-                  message: "Password length be greater than 8",
-                },
-              })}
-              required
-              variant="outlined"
-              fullWidth
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel htmlFor="confirmPassword">Confirm Password</FormLabel>
-            <TextField
-              error={!!errors?.confirmPassword} // Highlights the textfield with red border, if validation fails
-              helperText={errors?.confirmPassword?.message}
-              name="confirmPassword"
-              placeholder="Re enter new password"
-              type={showPassword ? "text" : "password"}
-              id="confirmPassword"
-              autoComplete="current-password"
-              {...register("confirmPassword", {
-                required: {
-                  value: true,
-                  message: "Please enter confirm password",
-                },
-                validate: (value) =>
-                  value === watch("newPassword") || "Passwords do not match",
-              })}
-              required
-              variant="outlined"
-              slotProps={{
-                input: {
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton
-                        onClick={() => setShowPassword(!showPassword)}
-                        edge="end"
-                        sx={{
-                          border: "none",
-                          outline: "none",
-                          "&:focus": { outline: "none" },
-                          "&:hover": { backgroundColor: "transparent" },
-                        }}
-                      >
-                        {showPassword ? <Visibility /> : <VisibilityOff />}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                },
-              }}
-              fullWidth
-            />
-          </FormControl>
-          <Button
-            id="reset-password-submit"
-            type="submit"
+  return (
+    <div className="reset-password-bg">
+      <Box
+        noValidate
+        component="form"
+        onSubmit={handleSubmit(handleFormSubmit)}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          paddingX: 4,
+          gap: 2,
+          width: "100%",
+          maxWidth: "450px",
+        }}
+      >
+        <FormControl>
+          <FormLabel htmlFor="newPassword">New Password</FormLabel>
+          <TextField
+            error={!!errors?.newPassword} // Highlights the textfield with red border, if validation fails
+            helperText={errors?.newPassword?.message}
+            name="newPassword"
+            placeholder="Enter new password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            {...register("newPassword", {
+              required: {
+                value: true,
+                message: "Please enter the password",
+              },
+              pattern: {
+                value: /^(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]+$/,
+                message:
+                  "Password must contain at least one digit and one special character",
+              },
+              minLength: {
+                value: 8,
+                message: "Password length be greater than 8",
+              },
+            })}
+            required
+            variant="outlined"
             fullWidth
-            variant="contained"
-            disabled={isSubmitting}
-            sx={{ backgroundColor: isSubmitting ? "grey.500" : "" }}
-          >
-            Submit
-          </Button>
-        </Box>
-        <Snackbar
-          anchorOrigin={{ vertical: "top", horizontal: "right" }}
-          open={openSnackbar}
-          autoHideDuration={6000}
-          onClose={handleSnackbarClose}
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel htmlFor="confirmPassword">Confirm Password</FormLabel>
+          <TextField
+            error={!!errors?.confirmPassword} // Highlights the textfield with red border, if validation fails
+            helperText={errors?.confirmPassword?.message}
+            name="confirmPassword"
+            placeholder="Re enter new password"
+            type={showPassword ? "text" : "password"}
+            id="confirmPassword"
+            autoComplete="current-password"
+            {...register("confirmPassword", {
+              required: {
+                value: true,
+                message: "Please enter confirm password",
+              },
+              validate: (value) =>
+                value === watch("newPassword") || "Passwords do not match",
+            })}
+            required
+            variant="outlined"
+            slotProps={{
+              input: {
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      onClick={() => setShowPassword(!showPassword)}
+                      edge="end"
+                      sx={{
+                        border: "none",
+                        outline: "none",
+                        "&:focus": { outline: "none" },
+                        "&:hover": { backgroundColor: "transparent" },
+                      }}
+                    >
+                      {showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              },
+            }}
+            fullWidth
+          />
+        </FormControl>
+        <Button
+          id="reset-password-submit"
+          type="submit"
+          fullWidth
+          variant="contained"
+          disabled={isSubmitting}
+          sx={{ backgroundColor: isSubmitting ? "grey.500" : "" }}
         >
-          <Alert
-            onClose={handleSnackbarClose}
-            severity={passwordResetSuccess ? "success" : "error"}
-            variant="filled"
-          >
-            {alertMessage}
-          </Alert>
-        </Snackbar>
-      </div>
-    );
-  }
-
-  // const handlePasswordVisibility = () => {
-  //   setShowPassword((prev) => !prev); // Toggle the showPassword state
-  // };
-
-  // const Card = styled(MuiCard)(({ theme }) => ({
-  //   display: "flex",
-  //   flexDirection: "column",
-  //   alignSelf: "center",
-  //   width: "100%",
-  //   padding: theme.spacing(4),
-  //   gap: theme.spacing(2),
-  //   margin: "auto",
-  //   [theme.breakpoints.up("sm")]: {
-  //     maxWidth: "450px",
-  //   },
-  // }));
-
-  // const SignInContainer = styled(Stack)(({ theme }) => ({
-  //   height: "calc((1 - var(--template-frame-height, 0)) * 100dvh)",
-  //   minHeight: "100%",
-  //   padding: theme.spacing(2),
-  //   [theme.breakpoints.up("sm")]: {
-  //     padding: theme.spacing(4),
-  //   },
-  //   "&::before": {
-  //     content: '""',
-  //     display: "block",
-  //     position: "absolute",
-  //     zIndex: -1,
-  //     inset: 0,
-  //   },
-  // }));
-
-//   return (
-//     <AppTheme>
-//       <CssBaseline enableColorScheme />
-
-//       <SignInContainer
-//         // sx={{ height: "100%" }}
-//         direction="column"
-//         alignContent="center"
-//         justifyContent="space-between"
-//         id="signInContainer"
-//       >
-//         <Card id="cardContainer" variant="outlined" className="">
-//           {/* <SitemarkIcon /> */}
-//           <Typography
-//             component="h1"
-//             variant="h4"
-//             sx={{
-//               width: "100%",
-//               fontSize: "clamp(2rem, 10vw, 2.15rem)",
-//               textAlign: "center",
-//             }}
-//           >
-//             Reset Password
-//           </Typography>
-//           <Box
-//             noValidate
-//             component="form"
-//             onSubmit={handleSubmit(handleFormSubmit)}
-//             sx={{
-//               display: "flex",
-//               flexDirection: "column",
-//               width: "100%",
-//               gap: 2,
-//             }}
-//           >
-//             <FormControl>
-//               <FormLabel htmlFor="newPassword">New Password</FormLabel>
-//               <TextField
-//                 error={!!errors?.newPassword} // Highlights the textfield with red border, if validation fails
-//                 helperText={errors?.newPassword?.message}
-//                 name="newPassword"
-//                 placeholder="Enter new password"
-//                 type="password"
-//                 id="newPassword"
-//                 autoComplete="current-password"
-//                 {...register("newPassword", {
-//                   required: {
-//                     value: true,
-//                     message: "Please enter the password",
-//                   },
-//                   pattern: {
-//                     value: /^(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]+$/,
-//                     message:
-//                       "Password must contain at least one digit and one special character",
-//                   },
-//                   minLength: {
-//                     value: 8,
-//                     message: "Password length be greater than 8",
-//                   },
-//                 })}
-//                 required
-//                 variant="outlined"
-//                 fullWidth
-//               />
-//             </FormControl>
-
-//             <FormControl>
-//               <FormLabel htmlFor="confirmPassword">Confirm Password</FormLabel>
-//               <TextField
-//                 error={!!errors?.confirmPassword} // Highlights the textfield with red border, if validation fails
-//                 helperText={errors?.confirmPassword?.message}
-//                 name="confirmPassword"
-//                 placeholder="Re enter new password"
-//                 type={showPassword ? "text" : "password"}
-//                 id="confirmPassword"
-//                 autoComplete="current-password"
-//                 {...register("confirmPassword", {
-//                   required: {
-//                     value: true,
-//                     message: "Please enter confirm password",
-//                   },
-//                   validate: (value) =>
-//                     value === watch("newPassword") || "Passwords do not match",
-//                 })}
-//                 required
-//                 variant="outlined"
-//                 slotProps={{
-//                   input: {
-//                     endAdornment: (
-//                       <InputAdornment position="end">
-//                         <IconButton
-//                           onClick={handlePasswordVisibility}
-//                           edge="end"
-//                           sx={{
-//                             border: "none",
-//                             outline: "none",
-//                             "&:focus": { outline: "none" },
-//                             "&:hover": { backgroundColor: "transparent" },
-//                           }}
-//                         >
-//                           {showPassword ? <Visibility /> : <VisibilityOff />}
-//                         </IconButton>
-//                       </InputAdornment>
-//                     ),
-//                   },
-//                 }}
-//                 fullWidth
-//               />
-//             </FormControl>
-//             <Button
-//               type="submit"
-//               fullWidth
-//               variant="contained"
-//               disabled={isSubmitting}
-//               sx={{ backgroundColor: isSubmitting ? "grey.500" : "" }}
-//             >
-//               {isSubmitting ? "Submitting" : "Submit"}
-//             </Button>
-//           </Box>
-//         </Card>
-//       </SignInContainer>
-
-//       <Snackbar
-//         anchorOrigin={{ vertical: "top", horizontal: "right" }}
-//         open={openSnackbar}
-//         autoHideDuration={6000}
-//         onClose={handleSnackbarClose}
-//       >
-//         <Alert
-//           onClose={handleSnackbarClose}
-//           severity="success"
-//           variant="filled"
-//         >
-//           {alertMessage}
-//         </Alert>
-//       </Snackbar>
-//     </AppTheme>
-//   );
-// }
+          Submit
+        </Button>
+      </Box>
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        open={openSnackbar}
+        autoHideDuration={6000}
+        onClose={handleSnackbarClose}
+      >
+        <Alert
+          onClose={handleSnackbarClose}
+          severity={passwordResetSuccess ? "success" : "error"}
+          variant="filled"
+        >
+          {alertMessage}
+        </Alert>
+      </Snackbar>
+    </div>
+  );
+}
 
 export default ResetPassword;
