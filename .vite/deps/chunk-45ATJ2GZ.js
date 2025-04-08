d7de5d39baccdb589ac22d6bdca0a69028e31c40
kirtiranjan78 <kirtir279@gmail.com>
2025-04-08 21:02:13 +0530

    done:dashboard ui

---
+++ .vite/deps/chunk-45ATJ2GZ.js
@@ -0,0 +1,20 @@
+import {
+  createSvgIcon
+} from "./chunk-5F6BXC2F.js";
+import {
+  require_jsx_runtime
+} from "./chunk-CVWYDCK2.js";
+import {
+  __toESM
+} from "./chunk-DC5AMYBS.js";
+
+// node_modules/@mui/icons-material/esm/KeyboardArrowDown.js
+var import_jsx_runtime = __toESM(require_jsx_runtime());
+var KeyboardArrowDown_default = createSvgIcon((0, import_jsx_runtime.jsx)("path", {
+  d: "M7.41 8.59 12 13.17l4.59-4.58L18 10l-6 6-6-6z"
+}), "KeyboardArrowDown");
+
+export {
+  KeyboardArrowDown_default
+};
+//# sourceMappingURL=chunk-45ATJ2GZ.js.map
